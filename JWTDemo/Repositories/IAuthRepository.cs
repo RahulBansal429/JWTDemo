350d7e2892450b11e130fe8e3f266067e1457644
Rahul Bansal <Rahul.Bansal@CenturyLink.com>
2021-05-20 17:06:48 +0530

    First Commit

---
+++ JWTDemo/Repositories/IAuthRepository.cs
@@ -0,0 +1,20 @@
+﻿using JWTDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTDemo.Repositories
+{
+    public interface IAuthRepository
+    {
+        Task Create(User user);
+        Task Create(Role role);
+        Task<User> Get(string userName);
+        Task Create(UserRole userRole);
+
+        Task<Role> GetRole(string roleName);
+
+        Task<IEnumerable<UserRole>> GetRoles(string userName);
+    }
+}
